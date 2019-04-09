6b6481f4595a9e4220da074ab6044db0cbae2005
Andrew Wiens <adwiens@gmail.com>
2019-04-09 18:20:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { range, random } from 'lodash';
+
+import { lineChart, Point2D } from './chart';
+
+const values: Point2D[][] = range(100).map(x => [{ x, y: x + random(50) }, { x, y: x + random(50) }]);
+const series: string[] = ['thing1', 'thing2'];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-vega-embed';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('graph') graph: ElementRef;
+  ngAfterViewInit() {
+    lineChart(this.graph.nativeElement, { values, series });
+  }
 }
